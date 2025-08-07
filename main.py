a73e3cfbd16c9698d29168f2061ebe36bad8677a
KinLab666 <nik.balabanov.98@inbox.ru>
2025-08-07 18:51:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(
     title="Система Электронного Документооборота",
-    description="API для управления документами и сотрудниками",
+    description="это веб-приложение на Python, реализованное с использованием фреймворка FastAPI, предназначенное для автоматизации управления документами в организации.",
     openapi_tags=metadata.tags_metadata,
     version="1.0"
 )
@@ -26,7 +26,7 @@ def get_or_404(db, model, item_id: int):
     return item
 
 # Запросы для сотрудников
-@app.post("/employees/", response_model=schemas.Employee, tags=["Сотрудники"], summary = "Создание сотрудника")
+@app.post("/employees/", response_model=schemas.Employee, tags=["Сотрудники"], summary = "Создание сотрудника", response_description=metadata.response_description_1)
 def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
     db_emp = models.Employee(**employee.dict())
     db.add(db_emp)
@@ -34,11 +34,11 @@ def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_
     db.refresh(db_emp)
     return db_emp
 
-@app.get("/employees/", response_model=List[schemas.Employee], tags=["Сотрудники"], summary = "Просмотр списка сотрудников")
+@app.get("/employees/", response_model=List[schemas.Employee], tags=["Сотрудники"], summary = "Просмотр списка сотрудников", response_description=metadata.response_description_1)
 def read_employees(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.Employee).offset(skip).limit(limit).all()
 
-@app.put("/employees/{emp_id}", response_model=schemas.Employee, tags=["Сотрудники"], summary = "Редактирование сотрудника")
+@app.put("/employees/{emp_id}", response_model=schemas.Employee, tags=["Сотрудники"], summary = "Редактирование сотрудника", response_description=metadata.response_description_1)
 def update_employee(emp_id: int, data: schemas.EmployeeUpdate, db: Session = Depends(get_db)):
     emp = get_or_404(db, models.Employee, emp_id)
     for key, value in data.dict().items():
@@ -47,7 +47,7 @@ def update_employee(emp_id: int, data: schemas.EmployeeUpdate, db: Session = Dep
     db.refresh(emp)
     return emp
 
-@app.patch("/employees/{emp_id}", response_model=schemas.Employee, tags=["Сотрудники"], summary = "Частичное редактирование сотрудника")
+@app.patch("/employees/{emp_id}", response_model=schemas.Employee, tags=["Сотрудники"], summary = "Частичное редактирование сотрудника", response_description=metadata.response_description_1)
 def partial_update_employee(emp_id: int, data: schemas.EmployeeUpdate, db: Session = Depends(get_db)):
     emp = get_or_404(db, models.Employee, emp_id)
     for key, value in data.dict(exclude_unset=True).items():
@@ -65,7 +65,7 @@ def delete_employee(emp_id: int, db: Session = Depends(get_db)):
     return {"detail": f"Сотрудник с id={emp_id} успешно удалён"}
 
 # Запросы для входящих документов
-@app.post("/incoming/", response_model=schemas.IncomingDocument, tags=["Входящие документы"], summary = "Создание входящего документа")
+@app.post("/incoming/", response_model=schemas.IncomingDocument, tags=["Входящие документы"], summary = "Создание входящего документа", response_description=metadata.response_description_2)
 def create_incoming(doc: schemas.IncomingDocumentCreate, db: Session = Depends(get_db)):
     db_doc = models.IncomingDocument(**doc.dict())
     db.add(db_doc)
@@ -73,11 +73,11 @@ def create_incoming(doc: schemas.IncomingDocumentCreate, db: Session = Depends(g
     db.refresh(db_doc)
     return db_doc
 
-@app.get("/incoming/", response_model=List[schemas.IncomingDocument], tags=["Входящие документы"], summary = "Просмотр списка входящих документов")
+@app.get("/incoming/", response_model=List[schemas.IncomingDocument], tags=["Входящие документы"], summary = "Просмотр списка входящих документов", response_description=metadata.response_description_2)
 def read_incoming(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.IncomingDocument).offset(skip).limit(limit).all()
 
-@app.put("/incoming/{doc_id}", response_model=schemas.IncomingDocument, tags=["Входящие документы"], summary = "Редактирование входящего документа")
+@app.put("/incoming/{doc_id}", response_model=schemas.IncomingDocument, tags=["Входящие документы"], summary = "Редактирование входящего документа", response_description=metadata.response_description_2)
 def update_incoming(doc_id: int, data: schemas.IncomingDocumentUpdate, db: Session = Depends(get_db)):
     doc = get_or_404(db, models.IncomingDocument, doc_id)
     for key, value in data.dict().items():
@@ -86,7 +86,7 @@ def update_incoming(doc_id: int, data: schemas.IncomingDocumentUpdate, db: Sessi
     db.refresh(doc)
     return doc
 
-@app.patch("/incoming/{doc_id}", response_model=schemas.IncomingDocument, tags=["Входящие документы"], summary = "Частичное редактирование входящего документа")
+@app.patch("/incoming/{doc_id}", response_model=schemas.IncomingDocument, tags=["Входящие документы"], summary = "Частичное редактирование входящего документа", response_description=metadata.response_description_2)
 def partial_update_incoming(doc_id: int, data: schemas.IncomingDocumentUpdate, db: Session = Depends(get_db)):
     doc = get_or_404(db, models.IncomingDocument, doc_id)
     for key, value in data.dict(exclude_unset=True).items():
@@ -103,7 +103,7 @@ def delete_incoming(doc_id: int, db: Session = Depends(get_db)):
     return {"detail": f"Входящий документ с id={doc_id} удалён"}
 
 # Запросы для исходящих документов
-@app.post("/outgoing/", response_model=schemas.OutgoingDocument, tags=["Исходящие документы"], summary = "Создание исходящего документа")
+@app.post("/outgoing/", response_model=schemas.OutgoingDocument, tags=["Исходящие документы"], summary = "Создание исходящего документа", response_description=metadata.response_description_3)
 def create_outgoing(doc: schemas.OutgoingDocumentCreate, db: Session = Depends(get_db)):
     db_doc = models.OutgoingDocument(**doc.dict())
     db.add(db_doc)
@@ -111,11 +111,11 @@ def create_outgoing(doc: schemas.OutgoingDocumentCreate, db: Session = Depends(g
     db.refresh(db_doc)
     return db_doc
 
-@app.get("/outgoing/", response_model=List[schemas.OutgoingDocument], tags=["Исходящие документы"], summary = "Просмотр списка исходящих документов")
+@app.get("/outgoing/", response_model=List[schemas.OutgoingDocument], tags=["Исходящие документы"], summary = "Просмотр списка исходящих документов", response_description=metadata.response_description_3)
 def read_outgoing(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.OutgoingDocument).offset(skip).limit(limit).all()
 
-@app.put("/outgoing/{doc_id}", response_model=schemas.OutgoingDocument, tags=["Исходящие документы"], summary = "Редактирование исходящего документа")
+@app.put("/outgoing/{doc_id}", response_model=schemas.OutgoingDocument, tags=["Исходящие документы"], summary = "Редактирование исходящего документа", response_description=metadata.response_description_3)
 def update_outgoing(doc_id: int, data: schemas.OutgoingDocumentUpdate, db: Session = Depends(get_db)):
     doc = get_or_404(db, models.OutgoingDocument, doc_id)
     if data.delivery_method and data.delivery_method not in ["email", "mail"]:
@@ -126,7 +126,7 @@ def update_outgoing(doc_id: int, data: schemas.OutgoingDocumentUpdate, db: Sessi
     db.refresh(doc)
     return doc
 
-@app.patch("/outgoing/{doc_id}", response_model=schemas.OutgoingDocument, tags=["Исходящие документы"], summary = "Частичное редактирование исходящего документа")
+@app.patch("/outgoing/{doc_id}", response_model=schemas.OutgoingDocument, tags=["Исходящие документы"], summary = "Частичное редактирование исходящего документа", response_description=metadata.response_description_3)
 def partial_update_outgoing(doc_id: int, data: schemas.OutgoingDocumentUpdate, db: Session = Depends(get_db)):
     doc = get_or_404(db, models.OutgoingDocument, doc_id)
     update_data = data.dict(exclude_unset=True)
@@ -146,7 +146,7 @@ def delete_outgoing(doc_id: int, db: Session = Depends(get_db)):
     return {"detail": f"Исходящий документ с id={doc_id} удалён"}
 
 # Запросы для служебных записок
-@app.post("/memos/", response_model=schemas.Memo, tags=["Служебные записки"], summary = "Создание служебной записки")
+@app.post("/memos/", response_model=schemas.Memo, tags=["Служебные записки"], summary = "Создание служебной записки", response_description=metadata.response_description_4)
 def create_memo(memo: schemas.MemoCreate, db: Session = Depends(get_db)):
     db_memo = models.Memo(**memo.dict())
     db.add(db_memo)
@@ -154,11 +154,11 @@ def create_memo(memo: schemas.MemoCreate, db: Session = Depends(get_db)):
     db.refresh(db_memo)
     return db_memo
 
-@app.get("/memos/", response_model=List[schemas.Memo], tags=["Служебные записки"], summary = "Просмотр списка служебных записок")
+@app.get("/memos/", response_model=List[schemas.Memo], tags=["Служебные записки"], summary = "Просмотр списка служебных записок", response_description=metadata.response_description_4)
 def read_memos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.Memo).offset(skip).limit(limit).all()
 
-@app.put("/memos/{memo_id}", response_model=schemas.Memo, tags=["Служебные записки"], summary = "Редактирование служебной записки")
+@app.put("/memos/{memo_id}", response_model=schemas.Memo, tags=["Служебные записки"], summary = "Редактирование служебной записки", response_description=metadata.response_description_4)
 def update_memo(memo_id: int, data: schemas.MemoUpdate, db: Session = Depends(get_db)):
     memo = get_or_404(db, models.Memo, memo_id)
     for key, value in data.dict().items():
@@ -167,7 +167,7 @@ def update_memo(memo_id: int, data: schemas.MemoUpdate, db: Session = Depends(ge
     db.refresh(memo)
     return memo
 
-@app.patch("/memos/{memo_id}", response_model=schemas.Memo, tags=["Служебные записки"], summary = "Частичное редактирование служебной записки")
+@app.patch("/memos/{memo_id}", response_model=schemas.Memo, tags=["Служебные записки"], summary = "Частичное редактирование служебной записки", response_description=metadata.response_description_4)
 def partial_update_memo(memo_id: int, data: schemas.MemoUpdate, db: Session = Depends(get_db)):
     memo = get_or_404(db, models.Memo, memo_id)
     for key, value in data.dict(exclude_unset=True).items():
@@ -184,7 +184,7 @@ def delete_memo(memo_id: int, db: Session = Depends(get_db)):
     return {"detail": f"Служебная записка с id={memo_id} удалена"}
 
 # Запросы для Отчетов
-@app.post("/reports/", response_model=schemas.Report, tags=["Отчеты сотрудников"], summary = "Создание отчета сотрудника")
+@app.post("/reports/", response_model=schemas.Report, tags=["Отчеты сотрудников"], summary = "Создание отчета сотрудника", response_description=metadata.response_description_5)
 def create_report(report: schemas.ReportCreate, db: Session = Depends(get_db)):
     db_report = models.Report(**report.dict())
     db.add(db_report)
@@ -192,11 +192,11 @@ def create_report(report: schemas.ReportCreate, db: Session = Depends(get_db)):
     db.refresh(db_report)
     return db_report
 
-@app.get("/reports/", response_model=List[schemas.Report], tags=["Отчеты сотрудников"], summary = "Просмотр списка отчетов сотрудников")
+@app.get("/reports/", response_model=List[schemas.Report], tags=["Отчеты сотрудников"], summary = "Просмотр списка отчетов сотрудников", response_description=metadata.response_description_5)
 def read_reports(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.Report).offset(skip).limit(limit).all()
 
-@app.put("/reports/{report_id}", response_model=schemas.Report, tags=["Отчеты сотрудников"], summary = "Редактирование отчета сотрудника")
+@app.put("/reports/{report_id}", response_model=schemas.Report, tags=["Отчеты сотрудников"], summary = "Редактирование отчета сотрудника", response_description=metadata.response_description_5)
 def update_report(report_id: int, data: schemas.ReportUpdate, db: Session = Depends(get_db)):
     report = get_or_404(db, models.Report, report_id)
     for key, value in data.dict().items():
@@ -205,7 +205,7 @@ def update_report(report_id: int, data: schemas.ReportUpdate, db: Session = Depe
     db.refresh(report)
     return report
 
-@app.patch("/reports/{report_id}", response_model=schemas.Report, tags=["Отчеты сотрудников"], summary = "Частичное редактирование отчета сотрудника")
+@app.patch("/reports/{report_id}", response_model=schemas.Report, tags=["Отчеты сотрудников"], summary = "Частичное редактирование отчета сотрудника", response_description=metadata.response_description_5)
 def partial_update_report(report_id: int, data: schemas.ReportUpdate, db: Session = Depends(get_db)):
     report = get_or_404(db, models.Report, report_id)
     for key, value in data.dict(exclude_unset=True).items():
@@ -222,7 +222,7 @@ def delete_report(report_id: int, db: Session = Depends(get_db)):
     return {"detail": f"Отчёт с id={report_id} удалён"}
 
 # Запросы для приказов
-@app.post("/orders/", response_model=schemas.Order, tags=["Приказы"], summary = "Создание приказа")
+@app.post("/orders/", response_model=schemas.Order, tags=["Приказы"], summary = "Создание приказа", response_description=metadata.response_description_6)
 def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
     db_order = models.Order(**order.dict())
     db.add(db_order)
@@ -230,11 +230,11 @@ def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
     db.refresh(db_order)
     return db_order
 
-@app.get("/orders/", response_model=List[schemas.Order], tags=["Приказы"], summary = "Просмотр списка приказов")
+@app.get("/orders/", response_model=List[schemas.Order], tags=["Приказы"], summary = "Просмотр списка приказов", response_description=metadata.response_description_6)
 def read_orders(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.Order).offset(skip).limit(limit).all()
 
-@app.put("/orders/{order_id}", response_model=schemas.Order, tags=["Приказы"], summary = "Редактирование приказа")
+@app.put("/orders/{order_id}", response_model=schemas.Order, tags=["Приказы"], summary = "Редактирование приказа", response_description=metadata.response_description_6)
 def update_order(order_id: int, data: schemas.OrderUpdate, db: Session = Depends(get_db)):
     order = get_or_404(db, models.Order, order_id)
     for key, value in data.dict().items():
@@ -243,7 +243,7 @@ def update_order(order_id: int, data: schemas.OrderUpdate, db: Session = Depends
     db.refresh(order)
     return order
 
-@app.patch("/orders/{order_id}", response_model=schemas.Order, tags=["Приказы"], summary = "Частичное редактирование приказа")
+@app.patch("/orders/{order_id}", response_model=schemas.Order, tags=["Приказы"], summary = "Частичное редактирование приказа", response_description=metadata.response_description_6)
 def partial_update_order(order_id: int, data: schemas.OrderUpdate, db: Session = Depends(get_db)):
     order = get_or_404(db, models.Order, order_id)
     for key, value in data.dict(exclude_unset=True).items():
--- metadata.py
+++ metadata.py
@@ -4,7 +4,7 @@ tags_metadata = [
         "description": "Сотрудники организации",
         "externalDocs": {
             "description": "Информация по запросам",
-            "url": "https://github.com/KinLab666/Test_Program_5",
+            "url": "https://github.com/KinLab666",
         },
     },
     {
@@ -12,7 +12,7 @@ tags_metadata = [
         "description": "Входящие документы организации",
         "externalDocs": {
             "description": "Информация по запросам",
-            "url": "https://github.com/KinLab666/Test_Program_5",
+            "url": "https://github.com/KinLab666",
         },
     },
     {
@@ -20,7 +20,7 @@ tags_metadata = [
         "description": "Исходящие документы организации",
         "externalDocs": {
             "description": "Информация по запросам",
-            "url": "https://github.com/KinLab666/Test_Program_5",
+            "url": "https://github.com/KinLab666",
         },
     },
     {
@@ -28,7 +28,7 @@ tags_metadata = [
         "description": "Служебные записки организации",
         "externalDocs": {
             "description": "Информация по запросам",
-            "url": "https://github.com/KinLab666/Test_Program_5",
+            "url": "https://github.com/KinLab666",
         },
     },
     {
@@ -36,7 +36,7 @@ tags_metadata = [
         "description": "Отчеты сотрудников организации",
         "externalDocs": {
             "description": "Информация по запросам",
-            "url": "https://github.com/KinLab666/Test_Program_5",
+            "url": "https://github.com/KinLab666",
         },
     },
     {
@@ -44,8 +44,55 @@ tags_metadata = [
         "description": "Приказы организации",
         "externalDocs": {
             "description": "Информация по запросам",
-            "url": "https://github.com/KinLab666/Test_Program_5",
+            "url": "https://github.com/KinLab666",
         },
     },
 
 ]
+response_description_1="""id: Номер (автоматически)
+\nfull_name: ФИО"
+\nposition: Должность
+\nemail: Адрес электронной почты
+\nphone: Номер телефона
+\nnote: Примечание
+\ncreated_at: Дата и время (автоматически)
+\nupdated_at: Дата и время изменения (автоматически)"""
+
+#description_employees_1="Создание сотрудника"
+
+response_description_2="""id: Номер (автоматически)
+\nsender_id: От кого пришло"
+\nposition: Должность
+\nsubject: Предмет письма
+\nresolution: Резолюция
+\nnote: Примечание
+\ncreated_at: Дата и время (автоматически)
+\nupdated_at: Дата и время изменения (автоматически)"""
+
+response_description_3="""id: Номер (автоматически)
+\nsender_id: Кому отправлено"
+\nsubject: Предмет письма
+\nresolution: Резолюция
+\nnote: Примечание
+\ncreated_at: Дата и время (автоматически)
+\nupdated_at: Дата и время изменения (автоматически)"""
+
+response_description_4="""id: Номер (автоматически)
+\nauthor_id: номер сотрудника отправителя
+\ncontent: содежание
+\nnote: Примечание
+\ncreated_at: Дата и время (автоматически)
+\nupdated_at: Дата и время изменения (автоматически)"""
+
+response_description_5="""id: Номер (автоматически)
+\nauthor_id: номер сотрудника отправителя
+\nnote: Примечание
+\ncreated_at: Дата и время (автоматически)
+\nupdated_at: Дата и время изменения (автоматически)"""
+
+response_description_6="""id: Номер (автоматически)
+\ncontent: содежание
+\nauthor_id: номер сотрудника отправителя
+\nnote: Примечание
+\ncreated_at: Дата и время (автоматически)
+\nupdated_at: Дата и время изменения (автоматически)"""
